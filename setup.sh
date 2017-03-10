771c3c479f8583b27e64d7dd970ab245996daac7
Yuuki Takada <yuuki.takada@groove-x.com>
2017-03-10 11:06:51 +0900

    copy tmux conf

--- setup.sh
+++ setup.sh
@@ -7,10 +7,13 @@ u="$USER"
 sudo apt-get update
 
 # install debian packages
-sudo apt-get install emacs sl xsel git
+sudo apt-get install emacs sl xsel git tmux
 
 # copy configuration file
 cp -rf /home/$u/configuration/.emacs.d /home/$u/
+
+# copy tmux.conf
+cp /home/$u/configuration/.tmux.conf /home/$u/
 
 # create directory
 mkdir /home/$u/workspace
