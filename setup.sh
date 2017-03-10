e79d07ba73108a28adc08055dac13d1d4fd7f791
Yuuki Takada <yuuki.takada@groove-x.com>
2017-03-10 10:50:31 +0900

    mkdir

--- setup.sh
+++ setup.sh
@@ -1,3 +1,14 @@
 #! /bin/sh
 
+# Variables
+u="$USER"
+
+# install debian packages
 sudo apt-get install emacs sl xsel git
+
+# copy configuration file
+cp -rf /home/$u/configuration/.emacs.d /home/$u/
+
+# create directory
+mkdir /home/$u/workspace
+mkdir /home/$u/remote
